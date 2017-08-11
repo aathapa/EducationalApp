ea0ed688cdf3c7f17eb70b2f8ce6e32e4502c018
Aayush Thapa <aayushthapa@Aayushs-MacBook-Pro.local>
2017-08-11 15:30:33 +0545

    segmented tabbar item added

---
+++ src/components/MyCourses/Completed.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+
+} from 'react-native';
+
+class Completed extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View>
+        <Text>Current</Text>
+      </View>
+    );
+  }
+}
+
+export default Completed;
