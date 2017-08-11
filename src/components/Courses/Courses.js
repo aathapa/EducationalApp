07d4c12c34392983c8ef5126ae88c44e665f4274
Aayush Thapa <aayushthapa@Aayushs-MacBook-Pro.local>
2017-08-11 15:29:28 +0545

    design and marketing courses used in container

--- src/components/Courses/Courses.js
+++ src/components/Courses/Courses.js
@@ -2,10 +2,16 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
-  FlatList
+  FlatList,
+  TouchableOpacity,
+  ScrollView
 } from 'react-native';
-import PopularCourcesData from '../../Data';
+import PopularData from '../../Data/PopularData';
+import Designdata from '../../Data/DesignData';
+
 import PopularCourses from './PopularCourses';
+import DesignCourses from './DesignCourses';
+import CardContentInfo from '../CardContentInfo/CardContentInfo';
 
 class Course extends Component {
   constructor(props) {
@@ -13,10 +19,30 @@ class Course extends Component {
   }
   render() {
     return (
-      <View>
-        <PopularCourses
-          data ={PopularCourcesData}
-        />
+      <View style={{ backgroundColor: '#fff', flex: 1 }}>
+        <ScrollView>
+          <View>
+            <CardContentInfo category="Popular" />
+            <View style={{ marginTop: 10 }} >
+              <PopularCourses
+                data={PopularData}
+              />
+            </View>
+          </View>
+          <View>
+            <CardContentInfo category="Design" />
+            <View style={{ marginTop: 10 }}>
+              <DesignCourses data={Designdata} />
+            </View>
+          </View>
+          <View >
+            <CardContentInfo category="Marketing" />
+            <View style={{ marginTop: 10 }}>
+              <DesignCourses data={Designdata} />
+            </View>
+          </View>
+        </ScrollView>
+        
       </View>
     );
   }
