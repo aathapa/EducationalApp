7c722b26eded21986a3eb70f10d538965f482345
Aayush Thapa <aayushthapa@Aayushs-MacBook-Pro.local>
2017-08-11 15:30:16 +0545

    segmented tab bar package used

--- src/components/MyCourses/MyCourses.js
+++ src/components/MyCourses/MyCourses.js
@@ -1,17 +1,59 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text
+  Text,
 } from 'react-native';
+
+import SegmentedControlTab from 'react-native-segmented-control-tab';
+import Current from './Current';
+import Ongoing from './Ongoing';
+import Completed from './Completed';
 
 class MyCourse extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      selectedIndex: 0,
+    };
   }
+
+  handleIndexChange = (index) => {
+    this.setState({
+      ...this.state,
+      selectedIndex: index,
+    });
+  }
+
+  renderSegmentContent() {
+    switch (this.state.selectedIndex){
+      case 0:
+        return (
+          <Current />
+        );  
+      case 1:
+        return (
+          <Ongoing />
+        ); 
+      case 2:
+        return (
+          <Completed />
+        ); 
+    }
+  }
+
   render() {
     return (
-      <View>
-        <Text>MyCourse</Text>
+      <View style={{flex: 1,backgroundColor: '#fff'}}>
+        <View style={{marginHorizontal: 16,marginTop: 10}}>
+          <SegmentedControlTab
+            values={['Current', 'Ongoing', 'Completed']}
+            selectedIndex={this.state.selectedIndex}
+            onTabPress={this.handleIndexChange}
+          />
+        </View>
+        <View>
+          {this.renderSegmentContent()}
+        </View>
       </View>
     );
   }
