3275f1acb13f58cd1f9727c0aecf1f94cf1db9d0
Aayush Thapa <aayushthapa@Aayushs-MacBook-Pro.local>
2017-08-13 15:12:00 +0545

    data  changed

--- src/Data/DesignData.js
+++ src/Data/DesignData.js
@@ -2,6 +2,6 @@ import { Images } from '../common';
 
 export default DesignData = [
   { title: 'UI Design Wireframes', image: Images.design1, price: '126', rating: '5' },
-  { title: 'Mobile Design Introduction', image: Images.design2, price: '189', rating: '4.8' },
+  { title: 'Mobile Design ', image: Images.design2, price: '189', rating: '4.8' },
   { title: 'Mobile UI Wireframes', image: Images.design3, price: '100', rating: '4.4' }
 ]
