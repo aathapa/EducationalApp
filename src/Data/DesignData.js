cbf837069536b28b5292d99fab677cb76e474a6f
Aayush Thapa <aayushthapa@Aayushs-MacBook-Pro.local>
2017-08-15 15:37:07 +0545

    data added

--- src/Data/DesignData.js
+++ src/Data/DesignData.js
@@ -1,7 +1,30 @@
 import { Images } from '../common';
 
 export default DesignData = [
-  { title: 'UI Design Wireframes', image: Images.design1, price: '126', rating: '5' },
-  { title: 'Mobile Design ', image: Images.design2, price: '189', rating: '4.8' },
-  { title: 'Mobile UI Wireframes', image: Images.design3, price: '100', rating: '4.4' }
+  {
+    name: 'Mark White',
+    title: 'UI Design Wireframes',
+    image: Images.design1,
+    price: '126',
+    rating: '5',
+    totalStudent: "9",
+    currentStudent: "5"
+  },
+  {
+    name: 'Mark Stark',
+    title: 'Mobile Design ',
+    image: Images.design2,
+    price: '189',
+    rating: '4.8',
+    totalStudent: "10", currentStudent: "9"
+  },
+  {
+    name: 'Mark Root',
+    title: 'Mobile UI Wireframes',
+    image: Images.design3,
+    price: '100',
+    rating: '4.4',
+    totalStudent: "6",
+    currentStudent: "4"
+  }
 ]
